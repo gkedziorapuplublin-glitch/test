72cc51216d635cd47770dc31ff85f94e354973e5
gkedziorapuplublin <g.kedziora.pup.lublin@gmail.com>
2025-10-20 09:24:55 +0200

    add two pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Link, Outlet } from 'react-router'
 import './App.css'
 
 function App() {
   return (
     <>
       <div>   
-      <h1>Test React PUP Lublin</h1>      
+      <h1>Test React PUP Lublin</h1>  
+        <Link to="/">Home</Link>
+        <p> </p>
+        <Link to="/about">About</Link>
+        <p></p>
+        <Outlet />    
       </div>     
     </>
   )
